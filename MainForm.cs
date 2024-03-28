e739ce70a7d9fe9a26cbfaba1f85ad73e5b3504e
RRC-Malcolm <103592453+Malcsta@users.noreply.github.com>
2024-03-28 17:41:55 -0500

    Add files via upload

--- MainForm.cs
+++ MainForm.cs
@@ -14,11 +14,14 @@ namespace Module5Wurdle
     public class MainForm : WurdleForm
     {
         private string solution;
+        private int guessCount;
+        private const int MaximumAllowableGuesses = 6;
 
         public MainForm() 
         {
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
+            this.BackColor = Color.DimGray;
 
             this.mnuNew.ShortcutKeys = (Keys)(Keys.Control | Keys.N);
             this.mnuExit.ShortcutKeys = (Keys)(Keys.Alt | Keys.F4);
@@ -36,6 +39,7 @@ namespace Module5Wurdle
                 letterLabel.Font = new Font("Arial", 8.25f, FontStyle.Bold);
                 letterLabel.Size = new Size(30, 30);
                 letterLabel.BorderStyle = BorderStyle.FixedSingle;
+                letterLabel.ForeColor = Color.White;
 
                 this.flpAlphabet.Controls.Add(letterLabel);
             }
@@ -58,29 +62,96 @@ namespace Module5Wurdle
             this.mnuAbout.Click += MnuAbout_Click;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MnuAbout_Click(object sender, EventArgs e)
         {
             new AboutForm().ShowDialog();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MnuExit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnEnter_Click(object sender, EventArgs e)
         {
+            string guess = this.txtGuess.Text.ToUpper();
+            string solution = this.solution.ToUpper();
 
-            //Not in dictionary
-            if (!WurdleDictionary.IsInDictionary(this.txtGuess.Text.ToLower()))
+            //Word is not in dictionary
+            if (!WurdleDictionary.IsInDictionary(guess))
             {
                 this.errorProvider.SetError(this.txtGuess, "Not in the word list.");
                 return;
             }
+
+            this.guessCount += 1;
+
+            foreach (char guessedLetter in guess)
+            {
+                Control[] controls = this.flpAlphabet.Controls.Find(guessedLetter.ToString(), true);
+                if (controls.Length > 0 )
+                {
+                    Label letterLabel = (Label)controls[0];
+                    letterLabel.Enabled = false;    
+                }
+            }
+
+            //Word is in dictionary
+            for (int index = 0; index < guess.Length; index++)
+            {
+                Color backgroundColor = Color.Gray;
+
+                //Is the current guess letter equal to the current solution letter
+                
+                if (guess[index] == solution[index])
+                {
+                    backgroundColor = Color.Green;
+                }
+                else
+                {
+                    int searchIndex = solution.IndexOf(guess[index]);
+
+                    if(searchIndex != -1)
+                    {
+                        backgroundColor = Color.Orange;
+                    }
+                }
+
+                AddGuessLetterToTheGuessPanel(guess[index], backgroundColor);
+               
+            }
+
+            this.txtGuess.Text = string.Empty;
+
+            if (guess.Equals(solution))
+            {
+                MessageBox.Show("Congratulations!", this.Text);
+                this.txtGuess.Enabled = false;
+            }
+            else if (this.guessCount == MaximumAllowableGuesses)
+            {
+                MessageBox.Show($"The word is {solution}.", this.Text);
+                this.txtGuess.Enabled = false;
+            }
         }
 
         /// <summary>
-        /// 
+        /// add documentation later .. 
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -108,7 +179,28 @@ namespace Module5Wurdle
         {
             this.solution = Module5Library.WurdleDictionary.GetSolution();
 
+            this.guessCount = 0;    
+
             Debug.WriteLine(this.solution);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="letter"></param>
+        /// <param name="backgroundColor"></param>
+        private void AddGuessLetterToTheGuessPanel(char letter, Color backgroundColor)
+        {
+            Label letterLabel = new Label();
+            letterLabel.Name = letter.ToString();
+            letterLabel.Text = letter.ToString();
+            letterLabel.TextAlign = ContentAlignment.MiddleCenter;
+            letterLabel.Font = new Font("Arial", 20f, FontStyle.Bold);
+            letterLabel.Size = new Size(50, 50);
+            letterLabel.BorderStyle = BorderStyle.FixedSingle;
+            letterLabel.ForeColor = Color.White;    
+            letterLabel.BackColor = backgroundColor;
+            this.flpGuesses.Controls.Add(letterLabel);
         }
     }
 }
